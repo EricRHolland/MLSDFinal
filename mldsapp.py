8d447a74a90e9432b76590f125324895c665e784
EricRHolland <86615280+EricRHolland@users.noreply.github.com>
2021-12-03 22:37:01 -0600

    adding back image

--- mldsapp.py
+++ mldsapp.py
@@ -52,8 +52,8 @@ st.header("Here are your selections:")
 
 # show the image to the user and blow it up to be the same width for consistency.
 chosen_image = filelocator_img[image_select]
-
-st.image(chosen_image, caption = 'This is the image you selected', use_column_width = True)
+imageshow = Image.open(chosen_image)
+st.image(imageshow, caption = 'This is the image you selected', use_column_width = True)
 
 #show the video that the user selected. Note that st.video doesnt have a caption function, so did it by hand
 chosen_video = filelocator_video[video_select]
