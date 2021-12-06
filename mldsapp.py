d71558d12b95264a40614dae001ab29b111bc04e
EricRHolland <86615280+EricRHolland@users.noreply.github.com>
2021-12-06 13:32:06 -0600

    committing sidebar changes

--- mldsapp.py
+++ mldsapp.py
@@ -7,49 +7,135 @@ Created on Fri Dec  3 11:45:41 2021
 
 import streamlit as st
 from PIL import Image
-import cv2
+
 import numpy as np
 import os
+import cv2
+face_cascade = cv2.CascadeClassifier('face_detector.xml')
+eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
+smile_cascade = cv2.CascadeClassifier('haarcascade_smile.xml')
 
-def load_image(image_file):
-	img = Image.open(image_file)
-	return img
+
+
+def face_detection(inputimg):
+    gray = cv2.cvtColor(img_array, cv2.COLOR_BGR2GRAY)
+	# Detect faces
+    faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+	# Draw rectangle around the faces
+    for (x, y, w, h) in faces:
+				 cv2.rectangle(img, (x, y), (x+w, y+h), (45, 45, 255), 3)
+    return img,faces  #faces is for counting the number of detected faces
+
+def smile_detection(inputimg):
+    gray = cv2.cvtColor(img_array, cv2.COLOR_BGR2GRAY)
+	# Detect faces
+    smiles = smile_cascade.detectMultiScale(gray, 1.1, 4)
+    for (ex,ey,ew,eh) in smiles:
+	        cv2.rectangle(img,(ex,ey),(ex+ew,ey+eh),(45, 45, 255),2)
+    return img
+
+def cartoon_detection(inputimg):
+    gray = cv2.cvtColor(img_array, cv2.COLOR_BGR2GRAY)
+	# Detect faces
+    gray = cv2.medianBlur(gray, 5)
+    edge_places = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 9, 9)
+    color_user = cv2.bilateralFilter(img, 9, 300, 300)
+    finalcart = cv2.bitwise_and(color_user, color_user, mask=edge_places)
+    return finalcart
+
+def eyes_detection(inputimg):
+    gray = cv2.cvtColor(img_array, cv2.COLOR_BGR2GRAY)
+	# Detect faces
+    eyes = eye_cascade.detectMultiScale(gray, 1.3, 5)
+    for (ex,ey,ew,eh) in eyes:
+	        cv2.rectangle(img,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+    return img
+
+def Blackwhiteoutline_detection(inputimg):
+    img = cv2.GaussianBlur(inputimg, (11, 11), 0)
+    cannoli = cv2.Canny(img, 100, 150)
+    return cannoli
+
+
+
 
 os.chdir("C:/Users/EricH/MachineLearning/MLSDFinal")
-y_n = ['Facial Recognition','Deepfake Generators']
-toggleupload = st.sidebar.selectbox("What functions do you want to explore?",y_n)
+mode = ['Facial Recognition','Deepfake Generators']
+toggleupload = st.sidebar.selectbox("What functions do you want to explore?",mode)
 
 if toggleupload == "Facial Recognition":
+    
+    #List of possible functions to call
+    features_to_detect = ["Original","Full Faces", "Eyes","Smile","Cartonize", "Black & White Outline"]
+    #removed original from above list because I show the original as the column width before the transform listing
+    
+    #First sidebar selection that will pop asking which function to call
+    feature_list = st.sidebar.selectbox("What facial feature do you want to detect?",features_to_detect)
+    
+    #overall streamlit title that shows on the main page instead of the sidebar
     st.title('Facial Recognition Demo App')
     st.header('Use your own photo!')
     st.markdown('Upload a photo below and a neural network will identify facial features!')
+    
+    #File uploader that gives us the new_image, used to create img and array that goes into functions
     new_image = st.file_uploader("Upload Images", type=["png","jpg","jpeg"])
+    
+    #url string if needed (delete later if functionality changes)
     urlsite = str(new_image)
+    
+    
     if new_image is not None:
         
-        #show the image to the use
-        st.image(new_image)
-        #import face xml file from github repo
-        img = cv2.imread(new_image, cv2.COLOR_RGB2YCrCb)
-        # Display YCbCr image
-        st.image(img, caption="YCbCr image")
-
-        face_cascade = cv2.CascadeClassifier('face_detector.xml')
-        eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
-        smile_cascade = cv2.CascadeClassifier('haarcascade_smile.xml')
-        new_img = cv2.imread(new_image)
-        img = cv2.cvtColor(new_img,cv2.COLOR_BGR2GRAY)
-        gray = cv2.cvtColor(new_img, cv2.COLOR_BGR2GRAY)
-    	# Detect face from the user upload
-        faces = face_cascade.detectMultiScale(gray, 1.1, 4)
-    	# Draw rectangle around the faces
-        for (x, y, w, h) in faces:
-    				 cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
-
+        #import again because for some reason it doesnt work otherwise.
+        import cv2
+        #show the image to the user as the input and convert it for functions
+        image = Image.open(new_image)
+        #show image here
+        st.image(image, caption='Input', use_column_width=True)
+        
+        #only way this worked was to use np array instead of Image package in Pillow
+        img_array = np.array(image)
+        
+        #writing image to a jpg if needed (Maybe delete if functionality changes?)
+        
+        # img_written = cv2.imwrite('out.jpg', cv2.cvtColor(img_array, cv2.COLOR_RGB2BGR))
+        
+        # this is the last call that gets pulled into every function below. Very important!
+        img = cv2.cvtColor(img_array,1)
+        
+        #faces is the best and standard for this, if nothing works before Wednesday just use this one.
+        if feature_list == "Full Faces":
+            result_imagef,numface = face_detection(img)
+            st.image(result_imagef, use_column_width=True) 
+            
+            #Use this as a counter for multiple face detection, couldve done just 1
+            #ripped from cv2.face_detection readme on github by the guy who made the xml files
+            #incredibly useful
+            st.header("**The algorithm found {} face(s)**".format(len(numface)))
+            
+        #Eyes will draw blue rectangle around them, be sure to test before publish.
+        elif feature_list == "Eyes":
+            result_imageeye = eyes_detection(img)
+            st.image(result_imageeye, use_column_width=True) 
+        
+        #smile is straightforward, same as eyes but uses different Scaler and XML file.
+        elif feature_list == "Smile":
+            result_imagesmile = smile_detection(img)
+            st.image(result_imagesmile, use_column_width=True)
+            
+        #make sure to run this one before     
+        elif feature_list == "Black & White Outline":
+            result_imagebw = Blackwhiteoutline_detection(img)
+            st.image(result_imagebw, use_column_width=True)
+            
+        #make sure to run this one before      
+        elif feature_list == "Cartonize":
+            result_imagecart = cartoon_detection(img)
+            st.image(result_imagecart, use_column_width=True)
+        #o
+    #catches people that won't upload an image from getting a blank screen
     else: 
-        st.write("Try uploading an image.")
-
-
+        st.markdown("Please upload an image of 1 or more people to begin.")
 
 elif toggleupload == 'Deepfake Generators':
     st.title('Eric Holland Deepfake Demo App')
@@ -153,8 +239,6 @@ elif toggleupload == 'Deepfake Generators':
     st.markdown("This is because the other images are part of the training files used to tune the GAN neural network.")
     st.markdown("The more training images, the easier the model deals with new variables like different angles or glasses.")
     st.header("Thank you for trying this deepfake simulator!")
-
-
 
 
 # originalImage = cv2.imread("test-image.jpg")
