b6affae0238942bc672ea97f64b9c1fef8b4b514
EricRHolland <86615280+EricRHolland@users.noreply.github.com>
2021-12-07 21:03:46 -0600

    games not over

--- mldsapp.py
+++ mldsapp.py
@@ -283,14 +283,13 @@ elif toggleupload == 'App Summary':
              "Since I didn't have any prior knowledge for image and video handling, this project was very challenging to learn and implement with independent study.")
     
     
-    st.header("Problems Encountered & Future Additions")
+    future_additions = '<p color:Blue; font-size: 40px;">Testing the color blue on text. </p>'
+    st.header(future_additions)
     original_title = '<p style="font-family:Courier; color:Blue; font-size: 20px;">Original image</p>'
     st.markdown(original_title, unsafe_allow_html=True)
     st.image(image, channels="BGR")
 
-    new_title = '<p style="font-family:sans-serif; color:Green; font-size: 42px; </p>'
-    st.markdown(new_title, unsafe_allow_html=True)
-        
+
 # originalImage = cv2.imread("test-image.jpg")
 # savedImage = cv2.imwrite("saved-test-image.jpg",originalImage)
 # If you simply want to copy an image file however, there is no need to load it into memory, you can simply copy the file, without using opencv:
