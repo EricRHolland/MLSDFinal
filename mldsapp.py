822e58955233a098c4d0171fc517023c9b414715
EricRHolland <86615280+EricRHolland@users.noreply.github.com>
2021-12-08 13:01:24 -0600

    adding more color to explanation

--- mldsapp.py
+++ mldsapp.py
@@ -232,9 +232,12 @@ elif toggleupload == 'Deepfake Generators':
     st.video(chosen_output)
     
     #final explanation and thanks for using to show the user.
-    st.markdown("Note that any preprocessed videos perform better than the raw images and videos of myself.")
-    st.markdown("This is because the other images are part of the training files used to tune the GAN neural network.")
-    st.markdown("The more training images, the easier the model deals with new variables like different angles or glasses.")
+    st.subheader("Why do some of the combinations perform better than others?")
+    st.write("This is because the other images are part of the training files used to tune the GAN neural network. Other reasons are that some pictures need to be resized down to 256x256 which can sometimes confuse the algorithm.",
+             "While camera quality has increased, the eyes and smile algorithm have remained essentially untouched for the past 5+ years. If you are feeding higher definition images into a machine not trained on them, it will result in errors.",
+             "All images perform worse on my video because my eyes are not fixed on a given point and this makes it way more difficult for the parser to identify how to set the eyes.")
+    st.subheader("Why does the algorithm struggle with people turning their heads?")
+    st.write("Just like the previous problem, it has to do with how the algorithm was trained. The training sample videos were always pointed forward and were focused on mapping the facial features correctly. When a target video looks to the side, it can warp the face causing the output to appear alien-like.")
     st.subheader("Thank you for trying this deepfake simulator!")
 
 
