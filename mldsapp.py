8bb57e63bc73a54fe14db09b26fd0fe4ecfce47e
EricRHolland <86615280+EricRHolland@users.noreply.github.com>
2021-12-08 13:08:01 -0600

    celeb A color

--- mldsapp.py
+++ mldsapp.py
@@ -237,7 +237,8 @@ elif toggleupload == 'Deepfake Generators':
              "While camera quality has increased, the eyes and smile algorithm have remained essentially untouched for the past 5+ years. If you are feeding higher definition images into a machine not trained on them, it will result in errors.",
              "All images perform worse on my video because my eyes are not fixed on a given point and this makes it way more difficult for the parser to identify how to set the eyes.")
     st.subheader("Why do the outputs struggle with people turning their heads?")
-    st.write("Just like the previous problem, it has to do with how the algorithm was trained. The training sample videos were always pointed forward and were focused on mapping the facial features correctly. When a target video looks to the side, it can warp the face causing the output to appear alien-like.")
+    st.write("Just like the previous problem, it has to do with how the algorithm was trained. The training sample videos were always pointed forward and were focused on mapping the facial features correctly. When a target video looks to the side, it can warp the face causing the output to appear alien-like.",
+             "In the past 3 years, there have been numerous datasets like CelebA that use over 200,000 images of celebrities that try to address this problem in future GAN models.")
     st.subheader("Thank you for trying this deepfake simulator!")
 
 
